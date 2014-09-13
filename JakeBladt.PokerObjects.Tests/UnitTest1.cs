78f2847e87de969d1497bc91189b55473bb2445e
Jake Bladt <jake@jakebladt.com>
2014-09-12 21:34:24 -0400

    test project

---
+++ JakeBladt.PokerObjects.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JakeBladt.PokerObjects.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
